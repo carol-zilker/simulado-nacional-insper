5a72d17fd815bcaa7e8a19b82bcdca3ff19663d3
Alexandre Fernandes <alexandrefernandes@Alexandres-MacBook-Pro.local>
2022-05-17 18:52:54 -0300

    v4

--- relatorio.py
+++ relatorio.py
@@ -2925,7 +2925,7 @@ if login_aluno != '':
     tabela_final = tabela_questoes_debate(tabela_debate,'Temas', 'Comunicação Assertiva - Resultado Individual', 'Comunicação Assertiva - Resultado Geral', 'Interação com pessoas - Resultado Individual', 'Interação com pessoas - Resultado Geral', 'Pensamento crítico - Resultado Individual', 'Pensamento crítico - Resultado Geral', cor_texto,cor_back)
         
     with st.container():
-            col1, col2, col3 = st.columns([4, 20, 8])
+            col1, col2, col3 = st.columns([6, 20, 8])
             with col1:
                 st.write("")
             with col2:
