db520fc91a2db5f293c58c19bcd2704c6c61f12c
Alexandre Fernandes <alexandrefernandes@Alexandres-MacBook-Pro.local>
2022-05-17 15:25:17 -0300

    v3

--- relatorio.py
+++ relatorio.py
@@ -2097,7 +2097,9 @@ if login_aluno != '':
         </div>
     </div>
     """  
-
+    for i in range(len(redacao_aluno_media['Nota na questão'])):
+        if redacao_aluno_media['Nota na questão'][i] == 0:
+            redacao_aluno_media['Nota na questão'][i] = - 250
     if len(redacao_tabela3['Status']) != 0:
 
         ### REDAÇÃO
